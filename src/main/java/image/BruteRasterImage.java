456b623fcb7f41d480cfac6d6fd5d4c5bf376d40
PaulineTeoulle <pa.teoulle@gmail.com>
2018-11-28 12:01:45 +0100

    Modif' du constructeur et ajout des méthodes setter et getter

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -3,35 +3,61 @@ package image;
 import javafx.scene.paint.Color;
 import util.Matrices;
 
-public class BruteRasterImage implements Image{
+public class BruteRasterImage implements Image  {
 
     private Color[][] pixels;
+    private int width;
+    private int height;
 
     public BruteRasterImage(Color color, int width, int height){
-        Color[][] pixels = new Color[height][width];
-        for (int row = 0 ;row < height ; row++)
-            for (int col = 0; col <width ;col++)
-                pixels[row][col] = color;
-
+        for (int row =0 ; row< height ; row++)
+            for (int col =0 ; col< width ; col++)
+                this.pixels[row][col] = color;
     }
 
     public BruteRasterImage(Color[][] colors){
         this.pixels=colors;
     }
 
+    public void createRepresentation(){
 
+    }
+
+    public void setPixelColor(Color color, int x, int y){
+        pixels[y][x] = color;
+    }
+
+    private void setPixelsColor(Color[][] pixels){
+         this.pixels= pixels;
+    }
+
+    private void setPixelsColor(Color color){
+        for(int row =0 ; row <pixels[0].length ; row++)
+            for(int col =0 ; col<pixels.length ; col++)
+                pixels[row][col] = color;
+    }
 
     @Override
     public Color getPixelColor(int x, int y){
         return pixels[y][x];
     }
+
     @Override
     public int getWidth(){
-        return pixels[0].length;
+        return width;
     }
+
     @Override
     public int getHeight(){
-        return pixels.length;
+        return height;
+    }
+
+    protected void setWidth(int width){
+        this.width=width;
+    }
+
+    protected void setHeight(int height){
+        this.height=height;
     }
 
 }
