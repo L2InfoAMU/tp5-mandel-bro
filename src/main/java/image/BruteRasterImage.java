18b45e32d2325308965fad5db415dcf35ffd3aec
PaulineTeoulle <pa.teoulle@gmail.com>
2018-11-28 11:13:30 +0100

    Ajout du 2ème constructeur

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -8,12 +8,18 @@ public class BruteRasterImage implements Image{
     private Color[][] pixels;
 
     public BruteRasterImage(Color color, int width, int height){
+        Color[][] pixels = new Color[height][width];
+        for (int row = 0 ;row < height ; row++)
+            for (int col = 0; col <width ;col++)
+                pixels[row][col] = color;
 
     }
 
     public BruteRasterImage(Color[][] colors){
         this.pixels=colors;
     }
+
+
 
     @Override
     public Color getPixelColor(int x, int y){
