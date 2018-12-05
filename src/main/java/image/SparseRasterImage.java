cad6bf40fd33a16447a427f49324d3d62a01988e
Simon Crouzet <simon.crouzet@laposte.net>
2018-12-05 10:54:30 +0100

    Création et complétion de la classe SparseRasterImage Complétion des tests de SparseRasterImage

--- src/main/java/image/SparseRasterImage.java
+++ src/main/java/image/SparseRasterImage.java
@@ -6,21 +6,45 @@ import javafx.scene.paint.Color;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SparseRasterImage extends HashMap {
+public class SparseRasterImage extends RasterImage {
 
-    public HashMap<Point, Color> pixelColor = new HashMap();
+    public HashMap<Point, Color> pixelColor;
 
     public SparseRasterImage(Color color, int width, int height){
+        setHeight(height);
+        setWidth(width);
+        createRepresentation();
         for (int x =0 ; x < width ; x++)
             for(int y =0 ; y < height; y++)
-                pixelColor.put(new Point(x,y), color);
+                setPixelColor(color, x, y);
 
     }
     public SparseRasterImage(Color[][] pixels){
-
+        setHeight(pixels.length);
+        setWidth(pixels[0].length);
+        createRepresentation();
+        for (int x =0 ; x < width ; x++)
+            for(int y =0 ; y < height; y++)
+                setPixelColor(pixels[x][y], x, y);
     }
 
-    public int getHeigth(){
+    public void createRepresentation() {
+        this.pixelColor = new HashMap<>();
+    }
 
+    public void setPixelColor(Color color, int x, int y) {
+        pixelColor.put(new Point(x,y), color);
+    }
+
+    public Color getPixelColor(int x, int y) {
+        return pixelColor.get(new Point(x,y));
+    }
+
+    private void setPixelsColor(Color[][] pixels) {
+        new SparseRasterImage(pixels);
+    }
+
+    private void setPixelsColor(Color color) {
+        new SparseRasterImage(color, getWidth(), getHeight());
     }
 }
