413a14f053034e6a944ac7e35446dedf9a496c7a
PaulineTeoulle <pa.teoulle@gmail.com>
2018-12-05 11:12:37 +0100

    Modification de la classe Rectangle (constructeur et implémentation des méthodes)

--- src/main/java/image/Rectangle.java
+++ src/main/java/image/Rectangle.java
@@ -5,21 +5,27 @@ import javafx.scene.paint.Color;
 
 public class Rectangle implements Shape {
 
-    Rectangle(int x, int y, int width, int height, Color color){
-        Point p1 = new Point(x, y);
-        Point p2 = new Point(x+width,y);
-        Point p3 = new Point(x, y+height);
-        Point p4 = new Point(x+width,y+height);
+    public Point origin;
+    public int width;
+    public int height;
+    public Color color;
+
+
+    public Rectangle(int x, int y, int width, int height, Color color){
+        this.origin = new Point(x,y);
+        this.width = width;
+        this.height = height;
+        this.color = color;
     }
 
 
     @Override
     public boolean contains(Point point){
-
+        return(point.x >= origin.x && point.y >= origin.y && point.x <= this.height && point.y <=this.width);
     }
 
     @Override
     public Color getColor(){
-
+        return this.color;
     }
 }
