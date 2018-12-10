824c49a9e5f1b26f23bce3a6e28255e4fc3a38f6
PaulineTeoulle <pa.teoulle@gmail.com>
2018-12-10 17:49:37 +0100

    Documentation de la classe RasterImage

--- src/main/java/image/RasterImage.java
+++ src/main/java/image/RasterImage.java
@@ -5,20 +5,32 @@ public abstract class RasterImage implements Image {
     protected int width;
     protected int height;
 
+    /*Fonction qui retourne la largeur de l'image
+     * @return la largeur de l'image
+     */
     @Override
     public int getWidth(){
         return width;
     }
 
+    /*Fonction qui retourne la longueur de l'image
+     * @return la longueur de l'image
+     */
     @Override
     public int getHeight(){
         return height;
     }
 
+    /*Fonction qui fixe la largeur de l'image
+     * @param la largeur de l'image que l'on veut
+     */
     protected void setWidth(int width){
         this.width=width;
     }
 
+    /*Fonction qui fixe la longueur de l'image
+     * @param la longueur de l'image que l'on veut
+     */
     protected void setHeight(int height){
         this.height=height;
     }
