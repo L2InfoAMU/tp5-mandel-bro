0a5fb64f3b9e36c004753d9f88378a15c26ea8fb
PaulineTeoulle <pa.teoulle@gmail.com>
2018-12-10 17:54:24 +0100

    Documentation de la classe Annulus

--- src/main/java/image/Annulus.java
+++ src/main/java/image/Annulus.java
@@ -7,17 +7,30 @@ public class Annulus implements Shape{
     public Disk outerDisk;
     public Color color;
 
+    /*Fonction qui construit un anneau de paramètres donnés
+     * @param innerDisk, le disque intérieur de l'anneau
+     * @param outerDisk, le disque extérieur de l'anneau
+     * @param color, la couleur de l'anneau
+     */
     public Annulus (Disk innerDisk, Disk outerDisk, Color color){
         this.innerDisk=innerDisk;
         this.outerDisk=outerDisk;
         this.color=color;
     }
 
+    /*Fonction qui permet de savoir si un point est dans l'anneau ou pas
+     * @param un point
+     * @return true si le point est contenu dans l'anneau, false sinon
+     */
     @Override
     public boolean contains(Point point) {
         return innerDisk.contains(point) && !(outerDisk.contains(point));
     }
 
+
+    /*Fonction qui retourne la couleur de l'anneau
+     * @return la couleur de l'anneau
+     */
     @Override
     public Color getColor() {
         return this.color;
