4860ad5940f4c0e03985caefb6c8adeb1cb2570e
PaulineTeoulle <pa.teoulle@gmail.com>
2018-12-10 18:01:12 +0100

    Documentation de la classe Pixel

--- src/main/java/image/Pixel.java
+++ src/main/java/image/Pixel.java
@@ -8,11 +8,19 @@ import javafx.scene.paint.Color;
 public class Pixel extends Point{
     private Color color;
 
+    /*Constructeur du pixel de paramètres donnés
+     * @param x, l'abscisse du pixel
+     * @param y, l'ordonnée du pixel
+     * @param color, la couleur voulu du pixel
+     */
     Pixel(int x, int y, Color color) {
         super(x, y);
         this.color = color;
     }
 
+    /*Fonction qui retourne la couleur du pixel
+     * @return la couleur du pixel
+     */
     public Color getColor() {
         return color;
     }
