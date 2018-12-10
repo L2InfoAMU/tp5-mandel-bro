a5aa710bd248fff8ec1aba399332d70ca4470693
PaulineTeoulle <pa.teoulle@gmail.com>
2018-12-10 17:40:10 +0100

    Documentation de la classe Rectangle

--- src/main/java/image/Rectangle.java
+++ src/main/java/image/Rectangle.java
@@ -10,7 +10,13 @@ public class Rectangle implements Shape {
     public int height;
     public Color color;
 
-
+    /*Fonction qui construit un rectangle au point donné, de taille spécifiée et de couleur donnée
+     * @param x, l'abscisse du point de départ du rectangle
+     * @param y, l'ordonnée du point de départ du rectangle
+     * @param height la longueur de l'image
+     * @param width, la largeur de l'image
+     * @param color, la couleur du rectangle
+     */
     public Rectangle(int x, int y, int width, int height, Color color){
         this.origin = new Point(x,y);
         this.width = width;
@@ -18,12 +24,18 @@ public class Rectangle implements Shape {
         this.color = color;
     }
 
-
+    /*Fonction qui permet de savoir si un point est dans le rectangle ou pas
+     * @param un point
+     * @return true si le point est contenu dans le rectangle, false sinon
+     */
     @Override
     public boolean contains(Point point){
         return(point.x >= origin.x && point.y >= origin.y && point.x <= (origin.x+this.width) && point.y <= (origin.y+this.height));
     }
 
+    /*Fonction qui retourne la couleur du rectangle
+     * @return la couleur du rectangle
+     */
     @Override
     public Color getColor(){
         return this.color;
