3f1f25e9a134960d92c93917f1016749b538c8ff
PaulineTeoulle <pa.teoulle@gmail.com>
2018-12-10 17:57:56 +0100

    Documentation de la classe Disk et amélioration du constructeur

--- src/main/java/image/Disk.java
+++ src/main/java/image/Disk.java
@@ -5,25 +5,35 @@ import javafx.scene.paint.Color;
 public class Disk implements Shape {
 
     public Point origin;
-    public int width;
-    public int height;
     public Color color;
     public double radius;
 
-    public Disk(int x, int y, int width, int height, Color color, double radius){
+    /*Fonction qui construit un disque de paramètres donnés
+     * @param x, l'abscisse de l'origine
+     * @param y, l'ordonnée de l'origine
+     * @param radius, le rayon du disque
+     * @param color, la couleur du disque
+     */
+    public Disk(int x, int y, Color color, double radius){
         this.origin = new Point(x,y);
-        this.width = width;
-        this.height = height;
         this.color = color;
         this.radius = radius;
     }
 
+    /*Fonction qui permet de savoir si un point est dans le disque ou pas
+     * @param un point
+     * @return true si le point est contenu dans le disque, false sinon
+     */
     @Override
     public boolean contains(Point point) {
         double d = (origin.x - point.x)*(origin.x - point.x) + (origin.y - point.y) * (origin.y - point.y);
         return (d<= radius * radius);
     }
 
+
+    /*Fonction qui retourne la couleur du disque
+     * @return la couleur du disque
+     */
     @Override
     public Color getColor() {
         return this.color;
