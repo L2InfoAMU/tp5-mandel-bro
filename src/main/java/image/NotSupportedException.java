fb458ba5df3a728e4e600cce85583e9cfde35cdf
PaulineTeoulle <pa.teoulle@gmail.com>
2018-12-10 17:58:57 +0100

    Documentation de la classe NotSupportedException

--- src/main/java/image/NotSupportedException.java
+++ src/main/java/image/NotSupportedException.java
@@ -2,6 +2,7 @@ package image;
 
 public class NotSupportedException extends RuntimeException {
 
+    /*Constructeur d'exception qui renvoie un message*/
     public NotSupportedException(String message) {
         super(message);
     }
